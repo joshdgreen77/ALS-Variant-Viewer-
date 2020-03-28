af589dec8736ace8b4a134757b36e3b16a07e0e1
Joshua Green <joshdgreen77@gmail.com>
2020-03-28 16:27:17 -0400

    Misc Testing

--- variant-fixer.R
+++ variant-fixer.R
@@ -2,10 +2,17 @@ library(tidyverse)
 library(stringr)
 library(data.table)
 setwd("/Users/greenjod/Documents/GitHub/ALS-Variant-Viewer-/clinvar_cache/")
+
+
 args <- commandArgs(trailingOnly = FALSE)
 print(args[6])
 
-df <- fread(input = as.character(args[6]))
+df <- fread(input = "ALS2_clinvar.csv")
+df2 <- df %>% distinct(VariationID,.keep_all = TRUE)
+
+
+
+
 
 # replaces all analogous of this p.Thy69=) with p.Thy69Thy
 df$Protein.Consequence<- gsub(pattern = "p\\.(\\w{3})(\\d*)=\\)",replacement = "p\\.\\1\\2\\1",x = df$Protein.Consequence)
@@ -13,6 +20,6 @@ df$Protein.Consequence<- gsub(pattern = "p\\.(\\w{3})(\\d*)=\\)",replacement = "
 # replace fs) with fs
 df$Protein.Consequence<-gsub(pattern = "(.*)fs\\)",replacement = "\\1fs",x = df$Protein.Consequence)
 
-write.csv(x = df,file = args[6])
+write_csv(x = df,file = args[6])
 
 
